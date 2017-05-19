6fd9da30aae7c7569d34866ab7e0cfa6fa09c901
mpcrocha <mpc_rocha@hotmail.com>
2017-05-19 00:35:01 -0300

    Add files via upload

--- src/examples/pathTracer/PathTracerSphereJSucuri.java
+++ src/examples/pathTracer/PathTracerSphereJSucuri.java
@@ -1,31 +1,270 @@
 package examples.pathTracer;
 
-import jsucuriinoserialize.DFGraph;
-import jsucuriinoserialize.Scheduler;
+import com.nativelibs4java.opencl.*;
+import examples.javaCLTest.ObjectCopyCL;
+import examples.sobel.SobelUtils;
+import jsucuriinoserialize.*;
+import org.bridj.Pointer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by marcos on 06/05/17.
+ * numWorkers numIters numSimultaneousIters maxNumSpheres outputImageWidth outputImageHeight
+ * maxScenesCreationSimultaneous
+ * 4 2 2 9 1280 720 4
  */
 public class PathTracerSphereJSucuri {
     public static void main(String args[]){
 
+
+        NodeFunction configurationScene = new NodeFunction() {
+            @Override
+            public Object f(Object[] in_operands) {
+                PathTracerUtils pathTracerUtils = new PathTracerUtils();
+                Pointer<Float> sphereScene = pathTracerUtils.setSpheres();
+
+                return sphereScene;
+
+            }
+        };
+
+        NodeFunction copySceneToGpu = new NodeFunction() {
+            @Override
+            public Object f(Object[] in_operands) {
+                System.out.println("cin");
+                Pointer<Float> sphereScenesPointer  = (Pointer<Float>)in_operands[0];
+                CLBuffer<Float> spheresSceneBuffer = (CLBuffer)in_operands[1];
+                ObjectCopyCL outputBufferEvent = (ObjectCopyCL)in_operands[2];
+                if(outputBufferEvent.getEvent() != null) {
+                    CLEvent previousKernelEvent = outputBufferEvent.getEvent();
+                    previousKernelEvent.waitFor();
+                }
+
+                CLContext context = (CLContext)in_operands[in_operands.length - 2];
+                CLQueue queue = (CLQueue) in_operands[in_operands.length-1];
+
+                CLEvent copyImageEv = spheresSceneBuffer.write(queue, sphereScenesPointer, false);
+                ObjectCopyCL imageBufferEvent = new ObjectCopyCL(spheresSceneBuffer, copyImageEv);
+                return imageBufferEvent;
+            }
+        };
+
+        NodeFunction executeKernel = new NodeFunction() {
+            @Override
+            public Object f(Object[] in_operands) {
+                System.out.println("kernel");
+
+                ObjectCopyCL sphereSceneBufferEvent = (ObjectCopyCL)in_operands[0];
+                CLBuffer<Float> sphereSceneBuffer = (CLBuffer<Float>)sphereSceneBufferEvent.getBuffer();
+                CLEvent copyInEvent = (CLEvent)sphereSceneBufferEvent.getEvent();
+
+                CLBuffer<Float> outputBuffer = (CLBuffer<Float>)in_operands[1];
+                CLKernel kernel = (CLKernel) in_operands[2];
+
+                int imageWidth = (Integer) in_operands[3];
+                int imageHeight = (Integer) in_operands[5];
+
+                CLQueue queue = (CLQueue)in_operands[in_operands.length-1];
+
+                long[] globalWorkSizes = new long[] { imageWidth * imageHeight};
+                long[] localWorkS = new long[]{64};
+
+                kernel.setArg(0, sphereSceneBuffer);
+                kernel.setArg(1, imageWidth);
+                kernel.setArg(2, imageHeight);
+                kernel.setArg(3, 9);
+                kernel.setArg(4, outputBuffer);
+
+                ObjectCopyCL imageOutputEvent = (ObjectCopyCL)in_operands[4];
+
+                if(imageOutputEvent.getEvent() != null) {
+                    CLEvent previousCopyOut = imageOutputEvent.getEvent();
+                    previousCopyOut.waitFor();
+                }
+
+                copyInEvent.waitFor();
+
+                CLEvent kernelEv = kernel.enqueueNDRange(queue, globalWorkSizes, localWorkS, null);
+                ObjectCopyCL imageBufferEvent = new ObjectCopyCL(outputBuffer, kernelEv);
+                return imageBufferEvent;
+
+            }
+        };
+
+        NodeFunction copyOutSceneFromGpu = new NodeFunction() {
+            @Override
+            public Object f(Object[] in_operands) {
+                System.out.println("cp out");
+                ObjectCopyCL imageOutputBufferEvent = (ObjectCopyCL)in_operands[0];
+                CLQueue queue = (CLQueue) in_operands[in_operands.length -1];
+
+                CLEvent kernelEv  = imageOutputBufferEvent.getEvent();
+                CLBuffer<Float>outputImageBuffer = (CLBuffer<Float>) imageOutputBufferEvent.getBuffer();
+
+
+                Pointer<Float> outputImagePointer = Pointer.allocateFloats(1280 * 720 * 3);
+                kernelEv.waitFor();
+                CLEvent copyOutEv = outputImageBuffer.read(queue, outputImagePointer, false);
+                ObjectCopyCL imagePointerEvent = new ObjectCopyCL(outputImagePointer, copyOutEv);
+                return imagePointerEvent;
+            }
+        };
+
+        NodeFunction writeImage = new NodeFunction() {
+            @Override
+            public Object f(Object[] in_operands) {
+                System.out.println("Write");
+                ObjectCopyCL imageOutputPointerEvent = (ObjectCopyCL)in_operands[0];
+                int numIter = (Integer)in_operands[1];
+                Pointer<Float> outputImagePointer = (Pointer<Float>) imageOutputPointerEvent.getPointer();
+                int imageWidth = (Integer) in_operands[2];
+                int imageHeight = (Integer) in_operands[3];
+                try {
+                    SobelUtils sobelUtils = new SobelUtils();
+
+                    CLEvent copyOutEv  = imageOutputPointerEvent.getEvent();
+                    copyOutEv.waitFor();
+
+                    sobelUtils.write(outputImagePointer, "imagemPathSphere_"+numIter+".ppm",
+                            imageWidth, imageHeight);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return 0;
+            }
+        };
+
         int numWorkers = new Integer(args[0]);
+        int numIters = new Integer(args[1]);
+        int numSimultaneousIters = new Integer(args[2]);
+        int maxNumSpheres = new Integer(args[3]);
+        int outputImageWidth = new Integer(args[4]);
+        int outputImageHeight = new Integer(args[5]);
+        int maxScenesCreationSimultaneous = new Integer(args[6]);
 
         DFGraph dfg = new DFGraph();
 
+        CLContext context = JavaCL.createBestContext(CLPlatform.DeviceFeature.GPU);
+        Node contextFeeder = new Feeder(context);
+        dfg.add(contextFeeder);
 
-        /*Add some Nodes
-        dfg.add(A);
-        dfg.add(B);
-        dfg.add(C);
-        dfg.add(D);
-        */
+        List<Feeder> queueFeederList = new ArrayList<Feeder>();
+        List<Feeder> inputBuferFeederList = new ArrayList<Feeder>();
+        List<Feeder> outputBuferFeederList = new ArrayList<Feeder>();
 
-        /*Add some edges
-        A.add_edge(C, 0);
-        B.add_edge(C, 1);
-        C.add_edge(D, 0);
-        */
+        Feeder feederToken = new Feeder(new ObjectCopyCL());
+        dfg.add(feederToken);
+
+        Feeder imageWidthFeeder = new Feeder(outputImageWidth);
+        dfg.add(imageWidthFeeder);
+
+        Feeder imageHeightFeeder = new Feeder(outputImageHeight);
+        dfg.add(imageHeightFeeder);
+
+        for(int i = 0; i < numSimultaneousIters; i++) {
+            CLQueue queue = context.createDefaultQueue();
+            queueFeederList.add(new Feeder(queue));
+            dfg.add(queueFeederList.get(i));
+            //TODO check write of output to avoid errors with different spheres nnumbers
+            Pointer<Float> imageInput = Pointer.allocateFloats(16 * maxNumSpheres);
+            CLBuffer<Float> inputImageBuffer = context.createBuffer(CLMem.Usage.Input,
+                    imageInput);
+            inputBuferFeederList.add(new Feeder(inputImageBuffer));
+            dfg.add(inputBuferFeederList.get(i));
+
+            Pointer<Float> imageOutput = Pointer.allocateFloats(outputImageWidth * outputImageHeight * 3);
+            CLBuffer<Float> outputImageBuffer = context.createBuffer(CLMem.Usage.Output, imageOutput);
+            outputBuferFeederList.add(new Feeder(outputImageBuffer));
+            dfg.add(outputBuferFeederList.get(i));
+        }
+
+        CLKernel render_kernel;
+        String source = null;
+        try {
+            source = new Scanner(new File("pathTracerSphere.cl")).useDelimiter("\\Z").next();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        render_kernel = context.createProgram(source).createKernel("render_kernel");
+        Feeder kernelFeeder = new Feeder(render_kernel);
+        dfg.add(kernelFeeder);
+
+        List<Node> configurateSceneNodes = new ArrayList<Node>();
+        List<Node> copyScenesNodes = new ArrayList<Node>();
+        List<Node> execKernelListNodes = new ArrayList<Node>();
+        List<Node> copyOutListNodes = new ArrayList<Node>();
+        List<Node> writeOutListNodes = new ArrayList<Node>();
+        List<Feeder> numIterFeederList = new ArrayList<Feeder>();
+
+        for(int i = 0; i<numIters ; i ++){
+            //Add some Nodes
+            Node configurateSceneNode =  new Node(configurationScene, 1);
+            configurateSceneNodes.add(configurateSceneNode);
+            dfg.add(configurateSceneNodes.get(i));
+
+            Node copyScenesNode =  new Node(copySceneToGpu, 5);
+            copyScenesNodes.add(copyScenesNode);
+            dfg.add(copyScenesNodes.get(i));
+
+            Node execKernelListNode =  new Node(executeKernel, 8);
+            execKernelListNodes.add(execKernelListNode);
+            dfg.add(execKernelListNodes.get(i));
+
+            Node copyOutListNode =  new Node(copyOutSceneFromGpu, 2);
+            copyOutListNodes.add(copyOutListNode);
+            dfg.add(copyOutListNodes.get(i));
+
+            Node writeOutListNode =  new Node(writeImage, 4);
+            writeOutListNodes.add(writeOutListNode);
+            dfg.add(writeOutListNodes.get(i));
+
+            Feeder numIterFeeder = new Feeder(i);
+            numIterFeederList.add(numIterFeeder);
+            dfg.add(numIterFeederList.get(i));
+
+            configurateSceneNodes.get(i).add_edge(copyScenesNodes.get(i), 0);
+            inputBuferFeederList.get(i % numSimultaneousIters).add_edge(copyScenesNodes.get(i), 1);
+            contextFeeder.add_edge(copyScenesNodes.get(i), 3);
+            queueFeederList.get(i % numSimultaneousIters).add_edge(copyScenesNodes.get(i), 4);
+
+            copyScenesNodes.get(i).add_edge(execKernelListNodes.get(i), 0);
+            outputBuferFeederList.get(i % numSimultaneousIters).add_edge(execKernelListNodes.get(i), 1);
+            kernelFeeder.add_edge(execKernelListNodes.get(i), 2);
+            imageWidthFeeder.add_edge(execKernelListNodes.get(i), 3);
+            imageHeightFeeder.add_edge(execKernelListNodes.get(i), 5);
+            contextFeeder.add_edge(execKernelListNodes.get(i), 6);
+            queueFeederList.get(i % numSimultaneousIters).add_edge(execKernelListNodes.get(i), 7);
+
+            execKernelListNodes.get(i).add_edge(copyOutListNodes.get(i), 0);
+            queueFeederList.get(i % numSimultaneousIters).add_edge(copyOutListNodes.get(i), 1);
+
+            copyOutListNodes.get(i).add_edge(writeOutListNodes.get(i), 0);
+            numIterFeederList.get(i).add_edge(writeOutListNodes.get(i), 1);
+            imageWidthFeeder.add_edge(writeOutListNodes.get(i), 2);
+            imageHeightFeeder.add_edge(writeOutListNodes.get(i), 3);
+
+            if(i < numSimultaneousIters){
+                feederToken.add_edge(copyScenesNodes.get(i), 2);
+                feederToken.add_edge(execKernelListNodes.get(i), 4);
+            }else{
+                execKernelListNodes.get(i - numSimultaneousIters).add_edge(copyScenesNodes.get(i), 2);
+                copyOutListNodes.get(i - numSimultaneousIters).add_edge(execKernelListNodes.get(i), 4);
+            }
+
+            if(i < maxScenesCreationSimultaneous){
+                feederToken.add_edge(configurateSceneNodes.get(i), 0);
+            }else{
+                copyOutListNodes.get(i - maxScenesCreationSimultaneous).add_edge(configurateSceneNodes.get(i), 0);
+            }
+        }
+
 
         //start scheduler
         Scheduler sched = new Scheduler(dfg, numWorkers, false);
---
+++ src/examples/pathTracer/PathTracerUtils.java
@@ -0,0 +1,207 @@
+package examples.pathTracer;
+
+import org.bridj.Pointer;
+
+/**
+ * Created by Jano on 17/05/2017.
+ */
+public class PathTracerUtils {
+    //TODO make this method acept the number of spheres and some type of configuration of scenes
+    //TODO separate the creation of walls and the spheres
+    public Pointer<Float> setSpheres() {
+        int numSpheres = 9;
+        Pointer<Float> sphere = Pointer.allocateFloats(16 * numSpheres);
+
+        //left wall
+        sphere.set(0, 200.0f);
+        sphere.set(1, 0.0f);
+        sphere.set(2, 0.0f);
+        sphere.set(3, 0.0f);
+
+        sphere.set(4, -200.6f);
+        sphere.set(5, 0.0f);
+        sphere.set(6, 0.0f);
+        sphere.set(7, 0.0f);
+
+        sphere.set(8, 0.75f);
+        sphere.set(9, 0.25f);
+        sphere.set(10, 0.25f);
+        sphere.set(11, 0.0f);
+
+        sphere.set(12, 0.0f);
+        sphere.set(13, 0.0f);
+        sphere.set(14, 0.0f);
+        sphere.set(15, 0.0f);
+
+        //right wall
+        sphere.set(16, 200.0f);
+        sphere.set(17, 0.0f);
+        sphere.set(18, 0.0f);
+        sphere.set(19, 0.0f);
+
+        sphere.set(20, 200.6f);
+        sphere.set(21, 0.0f);
+        sphere.set(22, 0.0f);
+        sphere.set(23, 0.0f);
+
+        sphere.set(24, 0.25f);
+        sphere.set(25, 0.25f);
+        sphere.set(26, 0.75f);
+        sphere.set(27, 0.0f);
+
+        sphere.set(28, 0.0f);
+        sphere.set(29, 0.0f);
+        sphere.set(30, 0.0f);
+        sphere.set(31, 0.0f);
+
+        //floor
+        sphere.set(32, 200.0f);
+        sphere.set(33, 0.0f);
+        sphere.set(34, 0.0f);
+        sphere.set(35, 0.0f);
+
+        sphere.set(36, 0.0f);
+        sphere.set(37, -200.4f);
+        sphere.set(38, 0.0f);
+        sphere.set(39, 0.0f);
+
+        sphere.set(40, 0.9f);
+        sphere.set(41, 0.8f);
+        sphere.set(42, 0.7f);
+        sphere.set(43, 0.0f);
+
+        sphere.set(44, 0.0f);
+        sphere.set(45, 0.0f);
+        sphere.set(46, 0.0f);
+        sphere.set(47, 0.0f);
+
+        //ceiling
+        sphere.set(48, 200.0f);
+        sphere.set(49, 0.0f);
+        sphere.set(50, 0.0f);
+        sphere.set(51, 0.0f);
+
+        sphere.set(52, 0.0f);
+        sphere.set(53, 200.4f);
+        sphere.set(54, 0.0f);
+        sphere.set(55, 0.0f);
+
+        sphere.set(56, 0.9f);
+        sphere.set(57, 0.8f);
+        sphere.set(58, 0.7f);
+        sphere.set(59, 0.0f);
+
+        sphere.set(60, 0.0f);
+        sphere.set(61, 0.0f);
+        sphere.set(62, 0.0f);
+        sphere.set(63, 0.0f);
+
+        //back wall
+        sphere.set(64, 200.0f);
+        sphere.set(65, 0.0f);
+        sphere.set(66, 0.0f);
+        sphere.set(67, 0.0f);
+
+        sphere.set(68, 0.0f);
+        sphere.set(69, 0.0f);
+        sphere.set(70, -200.4f);
+        sphere.set(71, 0.0f);
+
+        sphere.set(72, 0.9f);
+        sphere.set(73, 0.8f);
+        sphere.set(74, 0.7f);
+        sphere.set(75, 0.0f);
+
+        sphere.set(76, 0.0f);
+        sphere.set(77, 0.0f);
+        sphere.set(78, 0.0f);
+        sphere.set(79, 0.0f);
+
+        //front wall
+        sphere.set(80, 200.0f);
+        sphere.set(81, 0.0f);
+        sphere.set(82, 0.0f);
+        sphere.set(83, 0.0f);
+
+        sphere.set(84, 0.0f);
+        sphere.set(85, 0.0f);
+        sphere.set(86, 202.4f);
+        sphere.set(87, 0.0f);
+
+        sphere.set(88, 0.9f);
+        sphere.set(89, 0.8f);
+        sphere.set(90, 0.7f);
+        sphere.set(91, 0.0f);
+
+        sphere.set(92, 0.0f);
+        sphere.set(93, 0.0f);
+        sphere.set(94, 0.0f);
+        sphere.set(95, 0.0f);
+
+        //left sphere
+        sphere.set(96, 0.16f);
+        sphere.set(97, 0.0f);
+        sphere.set(98, 0.0f);
+        sphere.set(99, 0.0f);
+
+        sphere.set(100, -0.25f);
+        sphere.set(101, -0.24f);
+        sphere.set(102, -0.1f);
+        sphere.set(103, 0.0f);
+
+        sphere.set(104, 0.9f);
+        sphere.set(105, 0.8f);
+        sphere.set(106, 0.7f);
+        sphere.set(107, 0.0f);
+
+        sphere.set(108, 0.0f);
+        sphere.set(109, 0.0f);
+        sphere.set(110, 0.0f);
+        sphere.set(111, 0.0f);
+
+        //right sphere
+        sphere.set(112, 0.16f);
+        sphere.set(113, 0.0f);
+        sphere.set(114, 0.0f);
+        sphere.set(115, 0.0f);
+
+        sphere.set(116, 0.25f);
+        sphere.set(117, -0.24f);
+        sphere.set(118, 0.1f);
+        sphere.set(119, 0.0f);
+
+        sphere.set(120, 0.9f);
+        sphere.set(121, 0.8f);
+        sphere.set(122, 0.7f);
+        sphere.set(123, 0.0f);
+
+        sphere.set(124, 0.0f);
+        sphere.set(125, 0.0f);
+        sphere.set(126, 0.0f);
+        sphere.set(127, 0.0f);
+
+        //lightsource
+        sphere.set(128, 1.0f);
+        sphere.set(129, 0.0f);
+        sphere.set(130, 0.0f);
+        sphere.set(131, 0.0f);
+
+        sphere.set(132, 0.0f);
+        sphere.set(133, 1.36f);
+        sphere.set(134, 0.0f);
+        sphere.set(135, 0.0f);
+
+        sphere.set(136, 0.0f);
+        sphere.set(137, 0.0f);
+        sphere.set(138, 0.0f);
+        sphere.set(139, 0.0f);
+
+        sphere.set(140, 9.0f);
+        sphere.set(141, 8.0f);
+        sphere.set(142, 6.0f);
+        sphere.set(143, 0.0f);
+
+        return sphere;
+
+    }
+}
